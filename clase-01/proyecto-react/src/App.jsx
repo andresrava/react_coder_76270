b93a7fb2b3622ebf5cc9b4ba7ac6b77844895d9e
martin <tinbonino@gmail.com>
2024-10-28 21:31:09 -0300

    clase 1

---
+++ clase-01/proyecto-react/src/App.jsx
@@ -0,0 +1,14 @@
+
+import './App.css'
+import Bienvenida from './Bienvenida'
+function App() {
+
+
+  return (
+    <>
+      <Bienvenida/>
+    </>
+  )
+}
+
+export default App
---
+++ clase-01/proyecto-react/src/Bienvenida.jsx
@@ -0,0 +1,16 @@
+function Bienvenida() {
+
+  return (
+    <div>
+        <h2>Bienvenidos a la clase 1 de React</h2>
+        <h3>Aqui vamos a...</h3>
+        <ul>
+            <li>Aprender</li>
+            <li>Divertirnos</li>
+            <li>Crecer como personas</li>
+        </ul>
+    </div>
+  )
+}
+
+export default Bienvenida
